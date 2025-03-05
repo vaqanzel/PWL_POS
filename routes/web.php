426b5b3c80621b0f2a7bad25db8e7c08dca227be
vaqanzel <valengrehasta@gmail.com>
2025-03-05 11:36:06 +0700

    Praktikum 2-6

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,11 @@ Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 //Route::get('/user', [UserController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('user.tambah_simpan');
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('user.ubah');
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('user.ubah_simpan');
+Route::get('user/hapus/{id}', [UserController::class, 'hapus']);
+
+
+
