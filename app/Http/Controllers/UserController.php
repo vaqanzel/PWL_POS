9d518e658835c526254efa4ea42c49cf8cba8818
vaqanzel <valengrehasta@gmail.com>
2025-03-05 08:31:19 +0700

    Praktikum 2-4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
     public function index()
     {
-       $userCount = UserModel::where('level_id', 2)->count();
-       return view('user', ['data' => $userCount]);
+       $user = UserModel::firstOrNew(
+        [
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ]
+    );
+    $user->save();
+       return view('user', ['data' => $user]);
     }
 }
