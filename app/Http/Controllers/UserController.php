8c0646b83a4ecd6739e7e5745902403e7e76d3ef
vaqanzel <valengrehasta@gmail.com>
2025-03-17 22:48:21 +0700

    Modal ajax hapus data user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -262,6 +262,13 @@ class UserController extends Controller
         }
         return redirect('/');
     }
+    public function confirm_ajax(string $id)
+    {
+        $user = UserModel::find($id);
+
+        return view('user.confirm_ajax', ['user' => $user]);
+    }
+
 
 
     
