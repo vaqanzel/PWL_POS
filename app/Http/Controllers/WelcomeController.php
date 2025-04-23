9a876a75c4bbbb2efd017289879a74c4c4b6dd2f
vaqanzel <valengrehasta@gmail.com>
2025-04-23 09:06:28 +0700

    add Dashboard

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,20 +1,38 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Http\Request;
 
+use App\Models\BarangModel;
+use App\Models\KategoriModel;
+use App\Models\PenjualanDetailModel;
+use App\Models\PenjualanModel;
+use App\Models\SupplierModel;
+use App\Models\UserModel;
+use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function index()
-    {
-        $breadcrumb = (object) [
+    public function index(){
+        $breadcrumb = (object)[
             'title' => 'Selamat Datang',
-            'list'  => ['Home', 'Welcome']
+            'list' => ['Home', 'Welcome']
         ];
-
         $activeMenu = 'dashboard';
+        $jumlahUser = UserModel::count();
+        $totalPendapatan = PenjualanDetailModel::sum('harga');
+        $jumlahBarang = BarangModel::count();
+        $jumlahKategori = KategoriModel::count();
+        $jumlahSupplier = SupplierModel::count();
 
-        return view('welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+        return view('welcome', [
+            'breadcrumb' => $breadcrumb, 
+            'activeMenu' => $activeMenu, 
+            'jumlah_user' => $jumlahUser,
+            'total_pendapatan' => 'Rp' . number_format($totalPendapatan, 0, ',', '.') . ',00',
+            'jumlah_barang' => $jumlahBarang,
+            'jumlah_kategori' => $jumlahKategori,
+            'jumlah_supplier' => $jumlahSupplier,
+
+        ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,95 @@
 @extends('layouts.template')
 
 @section('content')
+    <!-- Default box -->
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Hallo {{auth()->user()->nama}}, Apa Kabar</h3>
 
-<div class="card">
-    <div class="card-header">
-        <h3 class="card-title">Halo, apa kabar!!!</h3>
-        <div class="card-tools"></div>
-    </div>
-    <div class="card-body">
-        Selamat datang semua, ini adalah halaman utama dari aplikasi ini.
-    </div>
-</div>
+            <div class="card-tools"></div>
+        </div>
+        <div class="card-body">
+            Selamat datang, ini halaman utama
+        </div>
 
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{$jumlah_user}}</h3>
+
+                            <p>Jumlah User</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="/user" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            {{-- <h3>53<sup style="font-size: 20px">%</sup></h3> --}}
+                            <h3>{{$total_pendapatan}}</h3>
+
+                            <p>Total Pendapatan</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-stats-bars"></i>
+                        </div>
+                        <a href="pendapatan" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>{{$jumlah_barang}}</h3>
+
+                            <p>Jumlah Barang</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-person-add"></i>
+                        </div>
+                        <a href="barang" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>{{$jumlah_kategori}}</h3>
+
+                            <p>Jumlah Kategori</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-pie-graph"></i>
+                        </div>
+                        <a href="kategori" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-primary">
+                        <div class="inner">
+                            <h3>{{$jumlah_supplier}}</h3>
+
+                            <p>Jumlah Supplier</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-pie-graph"></i>
+                        </div>
+                        <a href="supplier" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+            </div>
+        </div>
+    </div>
 @endsection
